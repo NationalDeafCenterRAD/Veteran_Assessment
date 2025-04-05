3b85e235597fde2740a84c9c52ee9630554adeff
Jonah Winninghoff <jonah.winninghoff@gmail.com>
2025-04-05 16:56:30 -0700

    Finished the analysis.

--- Scripts/Main.R
+++ Scripts/Main.R
@@ -2,10 +2,17 @@
 library(dplyr)
 library(tidyr)
 library(data.table)
-
+library(Hmisc)
 
 # Functions---------------------------------------------------------------------
 '%!in%' <- function(x,y)!('%in%'(x,y))
+
+## Make percentages look neat 
+perc_round<-function(x){ # Keep it from becoming 0.00%
+  result<-paste0(round(x*100,2),'%')
+  
+  return(result)
+}
 
 ## Percentage calculator
 percent <- function(df,x,...){
@@ -163,7 +170,8 @@ avg_metric <- function(df,type,metric,...){ # Create average for any metric
   result<-df%>%
     group_by(...)%>%
     summarise(`Sample size` = n())%>%
-    right_join(result, by = thestring)
+    right_join(result, by = thestring)%>%
+    rename(`income (in dollar)` = PWGTP)
   
   return(result)
 }
@@ -181,7 +189,7 @@ for(i in seq(1,80,1)){
 
 # Get data from local or File Transfer Protocol of Census Bureau ACS 1Y PUMS
 year_n <- 2023
-filepath <- paste0(getwd(),'/Raw Data/',year_n,'_5y_csv_pus.zip')
+filepath <- paste0(getwd(),'/Raw_Data/',year_n,'_5y_csv_pus.zip')
 
 if(!file.exists(filepath)){
   urlpath<-paste0('https://www2.census.gov/programs-surveys/acs/data/pums/',
@@ -206,7 +214,7 @@ if(!any(grep('.csv',unzipped))){
     names(part_df)<-toupper(names(part_df))
     part_df<-select(part_df, 
                     toupper(c("serialno","state","agep","ddrs","dear","deye",
-                              "dout","dphy","dratx","drem","drat",
+                              "dout","dphy","dratx","drem","drat","cow",
                               "fdearp","esr","schl","sch","schg","rac1p",
                               "hisp","sex","pernp","pincp","ssip","wkhp",
                               "wkwn","adjinc","pwgtp","drat","mil","nativity",
@@ -218,7 +226,7 @@ if(!any(grep('.csv',unzipped))){
   }
 }
 
-hdestfile <- paste(getwd(),'Raw Data','2023_5y_csv_hus.zip',sep='/')
+hdestfile <- paste(getwd(),'Raw_Data','2023_5y_csv_hus.zip',sep='/')
 hunzipped <- unzip(hdestfile)
 hvector<-grep('.csv',hunzipped)
 
@@ -239,4 +247,212 @@ df1<-mega_df%>%left_join(hmega_df, by = 'SERIALNO')
 remove(mega_df,hmega_df)
 
 # Add objects to gitignore list due to larger than 1 GB
-system("find . -size +500M -not -path '*/.*' | sed 's|^\\./||' >> .gitignore")
+# system("find . -size +500M -not -path '*/.*' | sed 's|^\\./||' >> .gitignore")
+
+# Clean up
+df1<-df1%>%
+  mutate(DEAR = ifelse(DEAR == '1', "deaf", "hearing"))%>%# DEAR recode
+  mutate(RAC1P = recode(RAC1P, '1' = 'White',             # RAC1P recode
+                               '2' = 'Black',
+                               '3' = 'Native American',
+                               '6' = 'Asian/Pacific Islander',
+                               '7' = 'Asian/Pacific Islander',
+                               '8' = 'Other Race/Multiracial',
+                               '9' = 'Other Race/Multiracial',
+                               .default = 'Native American'))%>%
+  mutate(HISP = ifelse(HISP == '1', 'Not Latine','Latine'))%>% # HISP recode
+  mutate(SEX = ifelse(SEX == '1','male','female'))%>%   # SEX recode
+  mutate(DEYE = recode(DEYE, '1' = 'Blind',             # DEYE recode
+                       '2' = 'Sighted'))%>%
+  mutate(DDRS = recode(DDRS, '1' = 'Yes',               # DDRS recode
+                       '2' = 'No'))%>%
+  mutate(DOUT = recode(DOUT, '1' = 'Yes',               # DOUT recode
+                       '2' = 'No'))%>%
+  mutate(DPHY = recode(DPHY, '1' = 'Yes',               # DPHY recode
+                       '2' = 'No'))%>%
+  mutate(DREM = recode(DREM, '1' = 'Yes',               # DREM recode
+                       '2' = 'No'))%>%
+  mutate(ESR = recode(ESR, '1' = 'employed',
+                      '3' = 'unemployed',
+                      '6' = 'notinLF', 
+                      .default = 'employed'))%>%
+  mutate(RACETH=ifelse(HISP == 'Latine','Latine',      # Create RACETH
+                ifelse(RAC1P == 'Black','Black',
+                ifelse(RAC1P == 'Asian/Pacific Islander',
+                                'Asian/Pacific Islander',
+                ifelse(RAC1P == 'Native American',
+                                'Native American',
+                ifelse(RAC1P == 'White','White',
+                                'Other Race/Multiracial'))))))%>%
+  mutate(SCH = ifelse(SCHG > 14, 'Enrolled',
+                      'Not Enrolled'))%>%
+  mutate(SCHL = ifelse(SCHL < 16,'no HS diploma', 
+                ifelse(SCHL <= 17,'HS diploma',
+                ifelse(SCHL < 20,'some college',
+                ifelse(SCHL == 20,'associate',
+                ifelse(SCHL == 21,'bachelor', 
+                ifelse(SCHL <= 22,'master',
+                ifelse(SCHL > 22, 'phd/dr',NA))))))))%>%
+  mutate(COW_original = COW,
+         COW = as.character(COW),
+         COW = ifelse(is.na(COW),'N/A',COW),
+         COW = recode(COW,
+                      '1' = 'For-profit',
+                      '2' = 'Non-profit',
+                      '3' = "Local gov't",
+                      '4' = "State gov't",
+                      '5' = "Federal gov't",
+                      '9' = "Unemployed",
+                      'N/A' = 'N/A',
+                      .default = 'Self-employed/Business'
+         ))%>%
+  mutate(isVeteran = ifelse(is.na(VPS),F,T))%>%
+  mutate(PLUS = ifelse((DDRS == 'Yes' |     # self-care difficulty
+                        DOUT == 'Yes' |   # independent living difficulty
+                        DPHY == 'Yes' |   # ambulatory difficulty
+                        DREM == 'Yes') &  # cognitive difficulty
+                        DEYE == 'Sighted',# deafdisabled only
+                       'disabled',
+                ifelse((DDRS == 'No' &    # deaf only
+                        DOUT == 'No' &
+                        DPHY == 'No' &
+                        DREM == 'No') & 
+                        DEYE == 'Sighted',
+                                'no disability',
+                ifelse(DEYE == 'Blind',
+                               'blind',NA))))%>%
+  mutate(HINCP = as.numeric(HINCP))%>%
+  mutate(PERNP = as.numeric(PERNP))
+
+# Employment--------------------------------------------------------------------
+## General
+result<-percent(df1,'ESR',DEAR,isVeteran,ESR)
+write.csv(result, file = 'Assets/Employment/general.csv')
+
+## Race
+result<-percent(df1,'ESR',DEAR,isVeteran,RACETH,ESR)
+write.csv(result, file = 'Assets/Employment/race.csv')
+
+## Gender
+result<-percent(df1,'ESR',DEAR,isVeteran,SEX,ESR)
+write.csv(result, file = 'Assets/Employment/gender.csv')
+
+## Disability
+result<-percent(df1,'ESR',DEAR,isVeteran,PLUS,ESR)
+write.csv(result, file = 'Assets/Employment/disability.csv')
+
+# Class of Worker---------------------------------------------------------------
+## General
+result<-percent(df1,'COW',DEAR,isVeteran,COW)
+write.csv(result, file = 'Assets/ClassOfWorker/general.csv')
+
+## Race
+result<-percent(df1,'COW',DEAR,isVeteran,RACETH,COW)
+write.csv(result, file = 'Assets/ClassOfWorker/race.csv')
+
+## Gender
+result<-percent(df1,'COW',DEAR,isVeteran,SEX,COW)
+write.csv(result, file = 'Assets/ClassOfWorker/gender.csv')
+
+## Disability
+result<-percent(df1,'COW',DEAR,isVeteran,PLUS,COW)
+write.csv(result, file = 'Assets/ClassOfWorker/disability.csv')
+
+# Type of Employment------------------------------------------------------------
+employed<-df1%>%
+  filter(ESR == 'employed')
+employed<-employed%>% # self employee and bizowner
+  mutate(selfEmp = ifelse(COW_original %in% c(6,7), 'Y', 'N'))%>%
+  mutate(bizOwner = ifelse(COW_original == 7, 'Y','N'))%>%
+  mutate(fullTime = ifelse(WKWN %in% c(50,51,52) & WKHP >= 35,'yes','any'))
+
+### Self-Employed
+## General
+result<-percent(employed,'selfEmp',DEAR,isVeteran,selfEmp)
+write.csv(result, file = 'Assets/TypeOfEmployment/SelfEmployed/general.csv')
+
+## Race
+result<-percent(employed,'selfEmp',DEAR,isVeteran,RACETH,selfEmp)
+write.csv(result, file = 'Assets/TypeOfEmployment/SelfEmployed/race.csv')
+
+## Gender
+result<-percent(employed,'selfEmp',DEAR,isVeteran,SEX,selfEmp)
+write.csv(result, file = 'Assets/TypeOfEmployment/SelfEmployed/gender.csv')
+
+## Disability
+result<-percent(employed,'selfEmp',DEAR,isVeteran,PLUS,selfEmp)
+write.csv(result, file = 'Assets/TypeOfEmployment/SelfEmployed/disability.csv')
+
+### Business owners
+## General
+result<-percent(employed,'bizOwner',DEAR,isVeteran,bizOwner)
+write.csv(result, file = 'Assets/TypeOfEmployment/BusinessOwners/general.csv')
+
+## Race
+result<-percent(employed,'bizOwner',DEAR,isVeteran,RACETH,bizOwner)
+write.csv(result, file = 'Assets/TypeOfEmployment/BusinessOwners/race.csv')
+
+## Gender
+result<-percent(employed,'bizOwner',DEAR,isVeteran,SEX,bizOwner)
+write.csv(result, file = 'Assets/TypeOfEmployment/BusinessOwners/gender.csv')
+
+## Disability
+result<-percent(employed,'bizOwner',DEAR,isVeteran,PLUS,bizOwner)
+write.csv(result, file = 'Assets/TypeOfEmployment/BusinessOwners/disability.csv')
+
+### Full-Time Workers
+## General
+result<-percent(employed,'fullTime',DEAR,isVeteran,fullTime)
+write.csv(result, file = 'Assets/TypeOfEmployment/FullTimeWorkers/general.csv')
+
+## Race
+result<-percent(employed,'fullTime',DEAR,isVeteran,RACETH,fullTime)
+write.csv(result, file = 'Assets/TypeOfEmployment/FullTimeWorkers/race.csv')
+
+## Gender
+result<-percent(employed,'fullTime',DEAR,isVeteran,SEX,fullTime)
+write.csv(result, file = 'Assets/TypeOfEmployment/FullTimeWorkers/gender.csv')
+
+## Disability
+result<-percent(employed,'fullTime',DEAR,isVeteran,PLUS,fullTime)
+write.csv(result, file = 'Assets/TypeOfEmployment/FullTimeWorkers/disability.csv')
+
+# Income------------------------------------------------------------------------
+### Individual Income
+forresult<-employed%>%
+  filter(fullTime == 'yes')
+
+##### Median
+## General
+result<-avg_metric(forresult,'median','PERNP',DEAR,isVeteran)
+write.csv(result,'Assets/Median_Income/Individual/general.csv')
+
+## Race
+result<-avg_metric(forresult,'median','PERNP',DEAR,isVeteran,RACETH)
+write.csv(result,'Assets/Median_Income/Individual/race.csv')
+
+## Gender
+result<-avg_metric(forresult,'median','PERNP',DEAR,isVeteran,SEX)
+write.csv(result,'Assets/Median_Income/Individual/gender.csv')
+
+## Disability
+result<-avg_metric(forresult,'median','PERNP',DEAR,isVeteran,PLUS)
+write.csv(result,'Assets/Median_Income/Individual/disability.csv')
+
+##### Mean
+## General
+result<-avg_metric(forresult,'mean','PERNP',DEAR,isVeteran)
+write.csv(result,'Assets/Mean_Income/Individual/general.csv')
+
+## Race
+result<-avg_metric(forresult,'mean','PERNP',DEAR,isVeteran,RACETH)
+write.csv(result,'Assets/Mean_Income/Individual/race.csv')
+
+## Gender
+result<-avg_metric(forresult,'mean','PERNP',DEAR,isVeteran,SEX)
+write.csv(result,'Assets/Mean_Income/Individual/gender.csv')
+
+## Disability
+result<-avg_metric(forresult,'mean','PERNP',DEAR,isVeteran,PLUS)
+write.csv(result,'Assets/Mean_Income/Individual/disability.csv')
+
